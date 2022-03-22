c25ff9c52a6812eaf408bb5f4785daa0023f838b
zaida-hammel <99354763+zaida-hammel@users.noreply.github.com>
2022-03-22 22:25:31 +0000

    added whitespace

--- main.cs
+++ main.cs
@@ -1,6 +1,6 @@
 // Created by: Zaida Hammel
 // Created on: March 2022
-//
+// 
 // This function states user's address input.
 
 using System;
@@ -11,14 +11,18 @@ class Program {
         // This function states user's address input.
         string streetName;
         int streetNumber;
+
         Console.WriteLine("This program will retell you your street name and street number.");
         Console.WriteLine("");
+
         Console.Write("Enter your street name: ");
         streetName = Console.ReadLine();
         Console.Write("Enter your street number: ");
         streetNumber = Convert.ToInt32(Console.ReadLine());
+
         Console.WriteLine("");
         Console.WriteLine("Your address is: " + streetName + " " + streetNumber + ".");
+
         Console.WriteLine("\nDone.");
     }
 }
