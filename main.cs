c16317923022075fc307531632bba895d7b0fe1b
zaida-hammel <99354763+zaida-hammel@users.noreply.github.com>
2022-03-22 21:56:48 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,23 @@
+using System;
+
+class Program {
+  public static void Main(string[] args)
+    {
+        // This function states user's address input.
+        string streetName;
+        int streetNumber;
+
+        Console.WriteLine("This program will retell you your street name and street number.");
+        Console.WriteLine("");
+
+        Console.Write("Enter your street name: ");
+        streetName = Console.ReadLine();
+        Console.Write("Enter your street number: ");
+        streetNumber = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("");
+        Console.WriteLine("Your address is: " + streetName + " " + streetNumber + ".");
+
+        Console.WriteLine("\nDone.");
+    }
+}
