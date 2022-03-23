680413927594512f179a6b595e868d339b934a79
zaida-hammel <99354763+zaida-hammel@users.noreply.github.com>
2022-03-23 16:27:09 +0000

    whitespace

--- main.cs
+++ main.cs
@@ -13,13 +13,13 @@ class Program {
         int streetNumber;
         Console.WriteLine("This program will retell you your street name and street number.");
         Console.WriteLine("");
-    
         Console.Write("Enter your street name: ");
         streetName = Console.ReadLine();
         Console.Write("Enter your street number: ");
         streetNumber = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("");
         Console.WriteLine("Your address is: " + streetName + " " + streetNumber + ".");
+
         Console.WriteLine("\nDone.");
     }
 }
