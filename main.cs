7a1b8ab51c1b8271c8db83cc12c6ebc063d8c0f8
zaida-hammel <99354763+zaida-hammel@users.noreply.github.com>
2022-03-24 16:51:50 +0000

    edited

--- main.cs
+++ main.cs
@@ -1,28 +1,28 @@
 
 // Created by: Zaida Hammel
 // Created on: March 2022
-// 
-// This function states user's address input.
-
+//
+// This program accepts user input
 using System;
 
-class Program {
-  public static void Main(string[] args)
+class Program
+{
+    public static void Main(string[] args)
     {
-        // This function states user's street name and street number.
-        string streetName;
+        // This function accepts user input
         int streetNumber;
+        string streetName;
 
-        Console.WriteLine("This program will retell you your street name and street number.");
+        Console.WriteLine("This program puts together a user's address.");
         Console.WriteLine("");
-
-        Console.Write("Enter your street name: ");
-        streetName = Console.ReadLine();
         Console.Write("Enter your street number: ");
         streetNumber = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter your street name: ");
+        streetName = Console.ReadLine();
+
 
         Console.WriteLine("");
-        Console.WriteLine("Your address is: " + streetName + " " + streetNumber + ".");
+        Console.WriteLine("Your address is: " + streetNumber + " " + streetName + ".");
 
         Console.WriteLine("\nDone.");
     }
